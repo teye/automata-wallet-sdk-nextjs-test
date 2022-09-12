5b5a430a36819641185035351f8df4254ba519d6
teye <6906654+teye@users.noreply.github.com>
2022-09-12 13:46:26 +0800

    add nextjs

---
+++ web/pages/_app.js
@@ -0,0 +1,9 @@
+import { WalletProvider } from '@automata-network/react-wallet-sdk'
+import '@automata-network/react-wallet-sdk/style.css'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
